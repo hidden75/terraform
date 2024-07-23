55b0a6a857f4fd87f687f2ecf0965603207a71a6
db <hidden_87@yahoo.com>
2024-07-23 14:12:14 +0000

    6th commit

--- locals.tf
+++ locals.tf
@@ -7,7 +7,7 @@ locals {
 
   resource_group_name = "db_resources"
 
-  location = "West US"
+  location = "East US"
 
 
 
@@ -17,5 +17,12 @@ locals {
   private_subnet_name       = "private"
   vm_network_interface_name = "db_network_interface"
   vm_security_group         = "db_security_group"
+
+
+  vm_name = "db-vm"
+
+  vm_admin_username = "adminuser"
+
+  vm_admin_password = "P@$$w0rd1234!"
 }
 
--- vm.tf
+++ vm.tf
@@ -108,3 +108,53 @@ resource "azurerm_network_interface_security_group_association" "db_security_gro
   network_security_group_id = azurerm_network_security_group.vm_security_group.id
 
 }
+
+
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/windows_virtual_machine#example-usage
+
+resource "azurerm_windows_virtual_machine" "db_vm" {
+
+  name = local.vm_name
+
+  location = local.location
+
+  resource_group_name = local.resource_group_name
+
+  size = "Standard_A1"
+
+  admin_username = local.vm_admin_username
+
+  admin_password = local.vm_admin_password
+
+  network_interface_ids = [
+
+    azurerm_network_interface.db_vm_network_interface.id,
+
+  ]
+
+
+
+  os_disk {
+
+    caching = "ReadWrite"
+
+    storage_account_type = "Standard_LRS"
+
+  }
+
+
+
+  source_image_reference {
+
+    publisher = "MicrosoftWindowsServer"
+
+    offer = "WindowsServer"
+
+    sku = "2019-Datacenter"
+
+    version = "latest"
+
+  }
+
+}
